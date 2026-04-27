ea712605e4332cf76dcab10da237c962fd322ab5
vidya <vidya671@gmsil.com>
2026-04-27 13:14:18 +0200

    change the model llm.py

--- app/llm.py
+++ app/llm.py
@@ -2,7 +2,7 @@ import subprocess
 
 def ask_llama(prompt):
     result = subprocess.run(
-        ["ollama", "run", "llama3"],
+        ["ollama", "run", "gemma3"], #gemma3
         input=prompt,
         text=True,
         capture_output=True,
